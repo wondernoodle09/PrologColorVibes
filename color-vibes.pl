ed5194dc726ec26b4bfac7bb6d1de6132529d920
wondernoodle09 <annikachavez44@gmail.com>
2025-01-03 22:51:35 -0800

    Initial commit

---
+++ color-vibes.pl
@@ -0,0 +1,17 @@
+% color-vibes.pl
+
+% example keywords: calm, cozy, energetic, romantic, mysterious
+% color-properties: hue?, warmth/cool, saturation, brightness
+
+% RULES
+
+color(red, [calm-10, cozy-30, energetic-85, romantic-100, mysterious-30]).
+color(orange, [calm-15, cozy-95, energetic-85, romantic-40, mysterious-15]).
+color(yellow, [calm-20, cozy-85, energetic-90, romantic-30, mysterious-10]).
+color(green, [calm-90, cozy-85, energetic-70, romantic-5, mysterious-90]).
+color(blue, [calm-100, cozy-40, energetic-60, romantic-20, mysterious-100]).
+color(purple, [calm-80, cozy-40, energetic-80, romantic-80, mysterious-90]).
+
+
+
+
