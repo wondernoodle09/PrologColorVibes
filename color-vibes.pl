8e6a951449a543e8dbf31af96504aab816bae432
wondernoodle09 <annikachavez44@gmail.com>
2025-01-03 22:53:54 -0800

    testing layout

--- color-vibes.pl
+++ color-vibes.pl
@@ -3,7 +3,9 @@
 % example keywords: calm, cozy, energetic, romantic, mysterious
 % color-properties: hue?, warmth/cool, saturation, brightness
 
-% RULES
+% FACTS
+
+% color hue keyword weights
 
 color(red, [calm-10, cozy-30, energetic-85, romantic-100, mysterious-30]).
 color(orange, [calm-15, cozy-95, energetic-85, romantic-40, mysterious-15]).
@@ -12,6 +14,7 @@ color(green, [calm-90, cozy-85, energetic-70, romantic-5, mysterious-90]).
 color(blue, [calm-100, cozy-40, energetic-60, romantic-20, mysterious-100]).
 color(purple, [calm-80, cozy-40, energetic-80, romantic-80, mysterious-90]).
 
+% RULES
 
 
 
